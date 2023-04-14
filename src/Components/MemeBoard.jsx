3f6158ef0004a3743c284d299ac43c07c0e4fa6f
sol <sol081105@gmail.com>
2023-04-15 00:18:35 +0200

    meme board

---
+++ src/Components/MemeBoard.jsx
@@ -0,0 +1,15 @@
+import './MemeBoard.css';
+
+const MemeBoard = () => {
+    return (
+        <div className="result-meme">
+            <figure id='exportable-meme'>
+                <p className="superior-text"></p>
+                <img src="" alt='meme'/>
+                <p className="inferior-text"></p>
+            </figure>
+        </div>
+    )
+}
+
+export default MemeBoard;
