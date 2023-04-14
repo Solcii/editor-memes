0bd812e0426fe4ec7edf6349fcff6efd0b3629f0
sol <sol081105@gmail.com>
2023-04-14 21:53:18 +0200

    app title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import AppTitle from './components/AppTitle';
 import Imgmeme from './components/Imgmeme';
+
 
 function App() {
   return (
     <div className="App">
+      <AppTitle/>
       <Imgmeme/>
     </div>
   );
---
+++ src/Components/AppTitle.js
@@ -0,0 +1,9 @@
+import './AppTitle.css'
+
+const AppTitle = () => {
+    return(
+        <div><h1 className="title">Editor de memes</h1></div>
+    )
+}
+
+export default AppTitle;
