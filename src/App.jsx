cc702b0bcdb84baadc9f9705cda7c4c1679e11c9
sol <sol081105@gmail.com>
2023-04-15 01:52:40 +0200

    change image

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,39 @@ import ImagesList from "./components/ImagesList";
 import MainToolsPanel from "./components/MainToolsPanel";
 import MemeBoard from "./components/MemeBoard";
 import TextEditor from "./components/TextEditor";
+import React, { useState } from "react";
 
 function App() {
+  const defaultConfigs = {
+    background: 'white',
+    fontFamily: 'Arial',
+    image: null,
+    topText: {
+      size: 12,
+      color: 'black',
+      align: 'center',
+      position: 10
+    },
+    bottomText: {
+      size: 12,
+      color: 'black',
+      align: 'center',
+      position: 10
+    }
+  }
+  const [memeConfig, setMemeConfig] = useState(defaultConfigs);
+
+  const setImage = (name) => {
+    setMemeConfig({...memeConfig,image:name});
+  }
+
   return (
     <div className="App">
       <AppTitle />
       <MainToolsPanel />
-      <ImagesList />
+      <ImagesList setImage={setImage} />
       <TextEditor />
-      <MemeBoard />
+      <MemeBoard configs={memeConfig} />
       <Footer />
     </div>
   );
